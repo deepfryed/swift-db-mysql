dba78a8251f9dad36caa57e71d4f45f4b93131b1
Bharanee Rathna <deepfryed@gmail.com>
2015-03-22 18:36:39 +1100

    move datetime code to shared repo.

--- ext/swift/db/mysql/datetime.h
+++ ext/swift/db/mysql/datetime.h
@@ -1,8 +1,1 @@
-#pragma once
-
-#include "common.h"
-#include <math.h>
-
-DLL_PRIVATE extern VALUE cSwiftDateTime;
-DLL_PRIVATE void init_swift_datetime();
-DLL_PRIVATE VALUE datetime_parse(VALUE klass, const char *data, size_t size);
+../../../../vendor/swift-db-datetime/src/swift/db/datetime.h
